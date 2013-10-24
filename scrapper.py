b1d33e4e97d0314e54bbaca1d6a1a9ee2248a3e5
Aleksey Komissarov <ad3002@gmail.com>
2013-10-25 02:29:33 +0400

    scrapper function

---
+++ scrapper.py
@@ -0,0 +1,29 @@
+import sys, os
+import urllib, urllib2
+from lxml import objectify
+
+def make_request(url):
+        """Makes a request to external resource."""
+        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+              'Accept-Encoding': 'none',
+              'Accept-Language': 'en-US,en;q=0.8',
+              'Connection': 'keep-alive'
+        }
+        req = urllib2.Request(url, headers=hdr)
+        print url
+        try:
+            page = urllib2.urlopen(req)
+        except urllib2.HTTPError, error:
+            print error.fp.read()
+        content = page.read()
+        return content
+
+
+data =  _request(url)
+
+root = objectify.fromstring(data)
+print min([x.price for x in root.quicklook.sell_orders.iterchildren()])
+print max([x.price for x in root.quicklook.buy_orders.iterchildren()])
+	
